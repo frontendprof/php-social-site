3617dc7940841aa34067ca54e4e06c4792aec34b
Abdumalik <abdumaliksharipov@gmail.com>
2024-11-06 01:11:30 -0500

    style: style login page

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" href="public/assets/images/favicon/instagram.ico" type="image/x-icon">
+	<link rel="stylesheet" href="public/css/register.css">
 	<title>Login || Instagram</title>
 </head>
 <body>
@@ -18,12 +19,12 @@
 				<div class="siteLogo">
 					<img src="public/assets/images/logo/instagram.png" alt="instagram logo">
 				</div>
-				<input type="text" placeholder="Email or Username" class="form--input name="username">
+				<input type="text" placeholder="Email or Username" class="form--input" name="username">
 				<div class="passContainer" >
 					<input type="password" placeholder="Password" class="form--input" name="password" id="password">
 					<span class="show_hide_text cursor_pointer" id="show_hide">Show</span>
 				</div>
-				<button class="button cursor_pointer" type="submit" name="submitBtn">Log in</button>
+				<button class="btn cursor_pointer" type="submit" name="submitBtn">Log in</button>
 				<span class="divide">Or</span>
 				<a href="#" class="pass_reset">Forgot password</a>
 			</form>
