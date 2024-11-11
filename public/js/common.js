0636df3172aa940b877bb176095b7ff5f0dbefae
Abdumalik <abdumaliksharipov@gmail.com>
2024-11-11 03:44:34 -0500

    feat: create slide functionality

--- public/js/common.js
+++ public/js/common.js
@@ -1,7 +1,18 @@
 const getId = (id) => document.getElementById(id);
+const getSl = (selector) => document.querySelector(selector);
 
 const password = getId("password");
 const showHide = getId("show_hide");
+
+const heroImg = getSl(".heroImg");
+
+let index = 0;
+const IMAGE_DATA = [
+  "http://localhost/instagram/public/assets/images/1.png",
+  "http://localhost/instagram/public/assets/images/2.png",
+  "http://localhost/instagram/public/assets/images/3.png",
+  "http://localhost/instagram/public/assets/images/4.png",
+];
 
 if (password) {
   showHide.addEventListener("click", function () {
@@ -14,4 +25,15 @@ if (password) {
       showHide.innerText = "Show";
     }
   });
+
+  function showSlides() {
+    const slider = () => {
+      index++;
+      heroImg.style.backgroundImage = `url(${IMAGE_DATA[index]})`;
+      if (index === 3) index = -1;
+    };
+    let timer = setInterval(slider, 3000);
+  }
+
+  showSlides();
 }
